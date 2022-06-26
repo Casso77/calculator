ff89f5b6aa14faadc91f9901a3d8fadbdfb145bb
Casso77 <chimgozirimejim@gmail.com>
2022-06-26 08:07:00 +0100

    structure defined

---
+++ calc.js
@@ -0,0 +1,29 @@
+// alert(connected);
+
+function add(){
+    console.log("+ clicked")
+}
+
+function subtract(){
+    console.log("- clicked")
+}
+
+function multiply(){
+    console.log("* clicked")
+}
+
+function divide(){
+    console.log("/ clicked")
+}
+
+function clear(){
+    console.log("Clear clicked")
+}
+
+function result(){
+    // verify that the code is connected
+    //console.log("= clicked")
+    //getting the numbers from the input boxes
+    const num1=document.getElementById("num1").value;
+    console.log("num1 is " + num1)
+}
