6337b8cdd2d3425e57f82e0f388ab7b01c0bbaff
Casso77 <chimgozirimejim@gmail.com>
2022-07-01 00:36:13 +0100

    if statements added to js

--- calc.js
+++ calc.js
@@ -24,6 +24,22 @@ function result(){
     // verify that the code is connected
     //console.log("= clicked")
     //getting the numbers from the input boxes
-    const num1=document.getElementById("num1").value;
+    const num1=parseInt(document.getElementById("num1").value);
+    const num2=parseInt(document.getElementById("num2").value);
+   
+    //if statement for when addition operation is selected
+    let result = num1 + num2
+    console.log("num1 is " + num1)
+
+    //if statement for when subtraction operation is selected
+    let result = num1 - num2
+    console.log("num1 is " + num1)
+
+    //if statement for when multiplication operation is selected
+    let result = num1 * num2
+    console.log("num1 is " + num1)
+
+    //if statement for when division operation is selected
+    let result = num1 / num2
     console.log("num1 is " + num1)
 }
